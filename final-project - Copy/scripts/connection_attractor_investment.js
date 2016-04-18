d8c21d72779d8bd3b603bfdb90240575bcb6649c
egunn <erica.gunn@gmail.com>
2016-04-18 15:17:05 -0400

    final cleanup and comments

--- final-project - Copy/scripts/connection_attractor_investment.js
+++ final-project - Copy/scripts/connection_attractor_investment.js
@@ -1,28 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//***************************************************************
-//Investments
-//***************************************************************
-
-var Investment = function(connection){
-    this.size = 5;
-    //this.frequency = this.
-}
-
 //***************************************************************
 //ATTRACTOR
 //***************************************************************
@@ -86,37 +61,35 @@ function CollisionLabels(investor, centerCompany) {
             //draw labels with company name and dollar amount
             push();
                                                    
-            translate(this.investor.pos.x,this.investor.pos.y);
-            
-            //lower left quadrant
-            if(this.investor.pos.x <= width/2 && this.investor.pos.y >= height/2){
-                textAlign(RIGHT);
-                translate((this.investor.defaultRadius+10)*cos(this.theta), (this.investor.defaultRadius+10)*sin(this.theta));// + .15));
-            }
-            //upper left quadrant
-            else if(this.investor.pos.x < width/2 && this.investor.pos.y < height/2){
-                textAlign(RIGHT);
-                translate((this.investor.defaultRadius+5)*cos(this.theta), (this.investor.defaultRadius+5)*sin(this.theta));// + .15));
-            }
-            //lower right quadrant
-            else if(this.investor.pos.x >= width/2 && this.investor.pos.y >= height/2){
-                textAlign(LEFT);
-                translate((this.investor.defaultRadius+10)*cos(this.theta), (this.investor.defaultRadius+10)*sin(this.theta));
-            }
-            //upper right quadrant
-            else if(this.investor.pos.x > width/2 && this.investor.pos.y < height/2){
-                textAlign(LEFT);
-                translate((this.investor.defaultRadius+5)*cos(this.theta), (this.investor.defaultRadius+5)*sin(this.theta));
-            }
-            //rotate(theta);
-            //}
+                translate(this.investor.pos.x,this.investor.pos.y);
 
-            noStroke();
-            textSize(11);
-            textFont(ralewayReg);
-            fill(70);
+                //lower left quadrant
+                if(this.investor.pos.x <= width/2 && this.investor.pos.y >= height/2){
+                    textAlign(RIGHT);
+                    translate((this.investor.defaultRadius+10)*cos(this.theta), (this.investor.defaultRadius+10)*sin(this.theta));// + .15));
+                }
+                //upper left quadrant
+                else if(this.investor.pos.x < width/2 && this.investor.pos.y < height/2){
+                    textAlign(RIGHT);
+                    translate((this.investor.defaultRadius+5)*cos(this.theta), (this.investor.defaultRadius+5)*sin(this.theta));// + .15));
+                }
+                //lower right quadrant
+                else if(this.investor.pos.x >= width/2 && this.investor.pos.y >= height/2){
+                    textAlign(LEFT);
+                    translate((this.investor.defaultRadius+10)*cos(this.theta), (this.investor.defaultRadius+10)*sin(this.theta));
+                }
+                //upper right quadrant
+                else if(this.investor.pos.x > width/2 && this.investor.pos.y < height/2){
+                    textAlign(LEFT);
+                    translate((this.investor.defaultRadius+5)*cos(this.theta), (this.investor.defaultRadius+5)*sin(this.theta));
+                }
 
-            text(this.investor.name, 0,0);//console.log(f.pos.x);
+                noStroke();
+                textSize(11);
+                textFont(ralewayReg);
+                fill(70);
+
+                text(this.investor.name, 0,0);
             
             pop();
         }
@@ -126,23 +99,12 @@ function CollisionLabels(investor, centerCompany) {
             if (selectedCompany && selectedLabels!= [] ){
 
                 for (var STEPS = 0; STEPS < 4; STEPS++) {
-
-                    //console.log(selectedLabels.length);
-                    //pairwise comparison - save unique pair  particle a and b to compare to see if they are    
-                    //intersecting. If so, adjust by precise amount needed to remove overlap.
                     for (var i = 0; i < selectedLabels.length - 1; i++) {
                         for (var j = i + 1; j < selectedLabels.length; j++) {
 
-                            //console.log('hello');
                             var pa = selectedLabels[i];
                             var pb = selectedLabels[j];
 
-
-                            //var ab = p5.Vector.sub(pb.pos, pa.pos);
-                            //console.log(ab.x + ' ' + ab.y);
-
-                            //console.log(pa.pos.y + ' ' + pb.pos.y + " " + sq(ab.y));
-                            //console.log(ab.y);
                             if (sq(pa.investor.pos.y-pb.investor.pos.y) < 196){
                                 if (pb.investor.pos.y > pa.investor.pos.y   &&  
                                     sq(pa.investor.pos.x-pb.investor.pos.x) < sq(textWidth(pb.investor.name) + textWidth(pa.investor.name))){  
@@ -161,8 +123,7 @@ function CollisionLabels(investor, centerCompany) {
                 }
                 
             }
-            
-            //this.draw();
+
         }
 
 }
